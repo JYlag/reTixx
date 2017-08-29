3d5b4f2b5fe2b4d199035016981d7577396f68a7
JYlag <joeylagan@gmail.com>
2017-08-28 20:43:26 -0700

    reTixx -- Beginning

---
+++ src/main/java/com/retixx/reTixx/ReTixxApplication.java
@@ -0,0 +1,12 @@
+package com.retixx.reTixx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReTixxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReTixxApplication.class, args);
+	}
+}
