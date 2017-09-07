461f99780ab54c962a31e57f543abfc1c54eb46f
JYlag <joeylagan@gmail.com>
2017-09-06 20:21:36 -0700

    Working controller + fragments/index

---
+++ src/main/java/com/retixx/reTixx/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.retixx.reTixx.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "retixx")
+public class IndexController {
+
+    @RequestMapping(value = "")
+    public String index (Model model) {
+
+        model.addAttribute("title", "ReTixx");
+
+        return "index";
+    }
+}
